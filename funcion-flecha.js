0e51209c3380de7012fa76e7996827573397ac55
Juanrinc <juanfeliperinc85@gmail.com>
2025-08-12 19:31:39 -0500

    aprendiendo como se hace la funcion flecha

---
+++ funcion-flecha.js
@@ -0,0 +1,12 @@
+function nombredelafuncion(n1,n2){
+    console.log("los numeros son: ",n1,n2)
+}
+const variable=()=>{
+
+}
+const imprimirnombre=()=>{
+    return "hola mundo"+nombre;
+
+}
+resultado=imprimirnombre("juan");
+console.log(resultado);
